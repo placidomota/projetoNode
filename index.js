2b16c24bdc008a2ae25328961c6caf302a6bc5aa
placido <placidobb.ac@gmail.com>
2022-04-22 22:14:01 -0500

    Primeiro Projeto nodeJs

---
+++ index.js
@@ -0,0 +1,21 @@
+//Instaciamos o objeto Date
+let data = new Date();
+
+// Utilizamos a instancia para recuperar a hora e minutos
+let hora = data.getHours();
+let min = data.getMinutes();
+
+
+let saudacao;
+if(hora <= 11){
+    saudacao= "Bom dia";
+}else if(hora <= 17){
+    saudacao= "Boa tarde";
+}else {
+    saudacao= "Boa noite.";
+}
+
+console.log("Ola" + saudacao);
+console.log("Agora são " + hora + " horas e " + min + " minutos.");
+
+
