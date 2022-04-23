6a87354547cbe35051c0c973da6bf9127dabe912
placido <placidobb.ac@gmail.com>
2022-04-22 22:50:01 -0500

    Retornando dados no formato  JSON com isso a aplicacao passa a atender totalmente ao padrao REST

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Olá Placido , bem vindo ao express');  
+    res.json({mensagem: 'Olá Placido , bem vindo ao express'});  
 });
 
 app.listen(8080, () => {
